461f9aaa5a6504709c075e67d14b131176feb584
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-18 19:11:24 +0700

    update

--- test/models/subDocument.js
+++ test/models/subDocument.js
@@ -51,8 +51,13 @@ describe('Test sub document', () => {
         assert.equal(user2.cars[2].branch, 'Madza');
     });
 
-    xit('Can add update a car', async () => {
-        
+    it('Can add update a car', async () => {
+        const user = await User.findById(userId);
+        const toyotaCar = user.cars.find(car => car._id.toString() === toyotaId.toString());
+        toyotaCar.update({ color: 'Blue' });
+        await user.save();
+        const user2 = await User.findById(userId);
+        console.log(user2);
     });
 
     it('Can remove car by id', async () => {
