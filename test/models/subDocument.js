43b463a2485401e81914ce7af95e4c661b2a1f8f
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-18 18:15:43 +0700

    xong

---
+++ test/models/subDocument.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const User = require('../../src/user');
+
+describe('Test sub document', () => {
+    it('Can create user with cars', async () => {
+        
+    });
+
+    it('Can add new car for user', async () => {
+        
+    });
+
+    it('Can add update a car', async () => {
+        
+    });
+
+    it('Can add remove car by id', async () => {
+        
+    });
+});
