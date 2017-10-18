71e3bc199216f46d9738047d3a26f34c2a257d71
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-18 18:26:32 +0700

    creaet

--- test/models/subDocument.js
+++ test/models/subDocument.js
@@ -3,18 +3,28 @@ const User = require('../../src/user');
 
 describe('Test sub document', () => {
     it('Can create user with cars', async () => {
+        const user = new User({
+            name: 'Pho',
+            cars: [
+                { branch: 'Toyota', color: 'black' },
+                { branch: 'Merc', color: 'white' }
+            ]
+        });
+        await user.save();
+        const user2 = await User.findById(user._id);
+        assert.equal(user2.name, 'Pho');
+        console.log(user2);
+    });
+
+    xit('Can add new car for user', async () => {
         
     });
 
-    it('Can add new car for user', async () => {
+    xit('Can add update a car', async () => {
         
     });
 
-    it('Can add update a car', async () => {
-        
-    });
-
-    it('Can add remove car by id', async () => {
+    xit('Can add remove car by id', async () => {
         
     });
 });
